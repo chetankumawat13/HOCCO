c4b288b9174fa1595c6725b57dc0e1e3516f62d9
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-05-19 12:33:46 +0530

    feat: add navigation bar component with styling

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../style/navbar.css'
+import { NavLink } from 'react-router'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src="/logo.svg" alt="" />
+        </div>
+        <div className='nav-links'>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='/ourLegacy'>Our legacy</NavLink>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='/ourBrands'>our brands</NavLink>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='/ourProducts'>our products</NavLink>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='/franchise'>franchise</NavLink>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='/joinTheClub'>join the club</NavLink>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='careers'>careers</NavLink>
+            <NavLink className={({isActive}) => isActive ? "active" :""} to='contact'>contact</NavLink>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
