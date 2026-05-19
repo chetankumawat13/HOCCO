777ef5d1fe6e07a2c2e63e82305aa899bcb096b2
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-05-19 12:32:10 +0530

    feat: initialize Vite and React project with routing and basic structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import './style/base.css'
+import Legacy from './pages/Legacy'
+import Brands from './pages/Brands'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ourLegacy' element={<Legacy />} />
+        <Route path='/ourBrands' element={<Brands />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
