3464bdae435b956ae540f156791cfeef81b8496e
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-05-19 15:17:52 +0530

    feat: add responsive navigation menu with remixicon integration

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,12 +10,14 @@ const NavBar = () => {
         </div>
         <div className='nav-links'>
             <NavLink className={({isActive}) => isActive ? "active" :""} to='/ourLegacy'>Our legacy</NavLink>
-            <NavLink className={({isActive}) => isActive ? "active" :""} to='/ourBrands'>our brands</NavLink>
             <NavLink className={({isActive}) => isActive ? "active" :""} to='/product'>our products</NavLink>
             <NavLink className={({isActive}) => isActive ? "active" :""} to='/franchise'>franchise</NavLink>
             <NavLink className={({isActive}) => isActive ? "active" :""} to='/joinTheClub'>join the club</NavLink>
             <NavLink className={({isActive}) => isActive ? "active" :""} to='careers'>careers</NavLink>
             <NavLink className={({isActive}) => isActive ? "active" :""} to='contact'>contact</NavLink>
+        </div>
+        <div className='r-nav'>
+            <i class="ri-menu-line"></i>
         </div>
     </div>
   )
